3955c17b72cce05c21729c6b6cb088610071bd89
Alex Blewitt <alex.blewitt@gmail.com>
2020-05-24 02:27:35 +0100

    Implementation of JVM

---
+++ src/main/java/com/bandlem/jvm/jvmulator/JVM.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2020, Alex Blewitt, Bandlem Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.bandlem.jvm.jvmulator;
+public class JVM {
+	private byte[] bytecode;
+	private int pc;
+	Stack stack = new Stack();
+	public void run() {
+		while (pc != bytecode.length) {
+			step();
+		}
+	}
+	public void setBytecode(final byte[] code) {
+		bytecode = code;
+	}
+	public void step() {
+		final byte code = bytecode[pc++];
+		switch (code) {
+		case Opcodes.NOP:
+			return;
+		// Constants
+		case Opcodes.ICONST_0:
+			stack.push(0);
+			return;
+		case Opcodes.ICONST_1:
+			stack.push(1);
+			return;
+		case Opcodes.ICONST_2:
+			stack.push(2);
+			return;
+		case Opcodes.ICONST_3:
+			stack.push(3);
+			return;
+		case Opcodes.ICONST_4:
+			stack.push(4);
+			return;
+		case Opcodes.ICONST_5:
+			stack.push(5);
+			return;
+		case Opcodes.ICONST_M1:
+			stack.push(-1);
+			return;
+		case Opcodes.FCONST_0:
+			stack.push(0F);
+			return;
+		case Opcodes.FCONST_1:
+			stack.push(1F);
+			return;
+		case Opcodes.FCONST_2:
+			stack.push(2F);
+			return;
+		case Opcodes.LCONST_0:
+			stack.push(0L);
+			return;
+		case Opcodes.LCONST_1:
+			stack.push(1L);
+			return;
+		case Opcodes.DCONST_0:
+			stack.push(0D);
+			return;
+		case Opcodes.DCONST_1:
+			stack.push(1D);
+			return;
+		// Addition
+		case Opcodes.IADD:
+			stack.push(stack.popInt() + stack.popInt());
+			return;
+		case Opcodes.FADD:
+			stack.push(stack.popFloat() + stack.popFloat());
+			return;
+		case Opcodes.LADD:
+			stack.push(stack.popLong() + stack.popLong());
+			return;
+		case Opcodes.DADD:
+			stack.push(stack.popDouble() + stack.popDouble());
+			return;
+		// Multiplication
+		case Opcodes.IMUL:
+			stack.push(stack.popInt() * stack.popInt());
+			return;
+		case Opcodes.FMUL:
+			stack.push(stack.popFloat() * stack.popFloat());
+			return;
+		case Opcodes.LMUL:
+			stack.push(stack.popLong() * stack.popLong());
+			return;
+		case Opcodes.DMUL:
+			stack.push(stack.popDouble() * stack.popDouble());
+			return;
+		// Division
+		case Opcodes.IDIV:
+			stack.push(stack.popInt() / stack.popInt());
+			return;
+		case Opcodes.FDIV:
+			stack.push(stack.popFloat() / stack.popFloat());
+			return;
+		case Opcodes.LDIV:
+			stack.push(stack.popLong() / stack.popLong());
+			return;
+		case Opcodes.DDIV:
+			stack.push(stack.popDouble() / stack.popDouble());
+			return;
+		// Remainder
+		case Opcodes.IREM:
+			stack.push(stack.popInt() % stack.popInt());
+			return;
+		case Opcodes.FREM:
+			stack.push(stack.popFloat() % stack.popFloat());
+			return;
+		case Opcodes.LREM:
+			stack.push(stack.popLong() % stack.popLong());
+			return;
+		case Opcodes.DREM:
+			stack.push(stack.popDouble() % stack.popDouble());
+			return;
+		// Subtraction
+		case Opcodes.ISUB:
+			stack.push(stack.popInt() - stack.popInt());
+			return;
+		case Opcodes.FSUB:
+			stack.push(stack.popFloat() - stack.popFloat());
+			return;
+		case Opcodes.LSUB:
+			stack.push(stack.popLong() - stack.popLong());
+			return;
+		case Opcodes.DSUB:
+			stack.push(stack.popDouble() - stack.popDouble());
+			return;
+		default:
+			throw new IllegalStateException("Unknown opcode: " + code);
+		}
+	}
+}
---
+++ src/test/java/com/bandlem/jvm/jvmulator/JVMTest.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2020, Alex Blewitt, Bandlem Ltd
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.bandlem.jvm.jvmulator;
+import static com.bandlem.jvm.jvmulator.Opcodes.DADD;
+import static com.bandlem.jvm.jvmulator.Opcodes.DCONST_0;
+import static com.bandlem.jvm.jvmulator.Opcodes.DCONST_1;
+import static com.bandlem.jvm.jvmulator.Opcodes.DDIV;
+import static com.bandlem.jvm.jvmulator.Opcodes.DMUL;
+import static com.bandlem.jvm.jvmulator.Opcodes.DREM;
+import static com.bandlem.jvm.jvmulator.Opcodes.DSUB;
+import static com.bandlem.jvm.jvmulator.Opcodes.FADD;
+import static com.bandlem.jvm.jvmulator.Opcodes.FCONST_0;
+import static com.bandlem.jvm.jvmulator.Opcodes.FCONST_1;
+import static com.bandlem.jvm.jvmulator.Opcodes.FCONST_2;
+import static com.bandlem.jvm.jvmulator.Opcodes.FDIV;
+import static com.bandlem.jvm.jvmulator.Opcodes.FMUL;
+import static com.bandlem.jvm.jvmulator.Opcodes.FREM;
+import static com.bandlem.jvm.jvmulator.Opcodes.FSUB;
+import static com.bandlem.jvm.jvmulator.Opcodes.IADD;
+import static com.bandlem.jvm.jvmulator.Opcodes.ICONST_0;
+import static com.bandlem.jvm.jvmulator.Opcodes.ICONST_1;
+import static com.bandlem.jvm.jvmulator.Opcodes.ICONST_2;
+import static com.bandlem.jvm.jvmulator.Opcodes.ICONST_3;
+import static com.bandlem.jvm.jvmulator.Opcodes.ICONST_4;
+import static com.bandlem.jvm.jvmulator.Opcodes.ICONST_5;
+import static com.bandlem.jvm.jvmulator.Opcodes.ICONST_M1;
+import static com.bandlem.jvm.jvmulator.Opcodes.IDIV;
+import static com.bandlem.jvm.jvmulator.Opcodes.IMUL;
+import static com.bandlem.jvm.jvmulator.Opcodes.IREM;
+import static com.bandlem.jvm.jvmulator.Opcodes.ISUB;
+import static com.bandlem.jvm.jvmulator.Opcodes.LADD;
+import static com.bandlem.jvm.jvmulator.Opcodes.LCONST_0;
+import static com.bandlem.jvm.jvmulator.Opcodes.LCONST_1;
+import static com.bandlem.jvm.jvmulator.Opcodes.LDIV;
+import static com.bandlem.jvm.jvmulator.Opcodes.LMUL;
+import static com.bandlem.jvm.jvmulator.Opcodes.LREM;
+import static com.bandlem.jvm.jvmulator.Opcodes.LSUB;
+import static com.bandlem.jvm.jvmulator.Opcodes.NOP;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+class JVMTest {
+	private void expect(final double result, final byte[] code) {
+		final JVM jvm = run(code);
+		assertEquals(result, jvm.stack.pop().doubleValue());
+		assertThrows(IndexOutOfBoundsException.class, jvm.stack::peek);
+	}
+	private void expect(final float result, final byte[] code) {
+		final JVM jvm = run(code);
+		assertEquals(result, jvm.stack.pop().floatValue());
+		assertThrows(IndexOutOfBoundsException.class, jvm.stack::peek);
+	}
+	private void expect(final int result, final byte[] code) {
+		final JVM jvm = run(code);
+		assertEquals(result, jvm.stack.pop().intValue());
+		assertThrows(IndexOutOfBoundsException.class, jvm.stack::peek);
+	}
+	private void expect(final long result, final byte[] code) {
+		final JVM jvm = run(code);
+		assertEquals(result, jvm.stack.pop().longValue());
+		assertThrows(IndexOutOfBoundsException.class, jvm.stack::peek);
+	}
+	private JVM run(final byte[] code) {
+		final JVM jvm = new JVM();
+		jvm.setBytecode(code);
+		jvm.run();
+		return jvm;
+	}
+	@Test
+	void testDouble() {
+		expect(1.0D, new byte[] {
+				DCONST_0, DCONST_1, DADD
+		});
+		expect(-1.0D, new byte[] {
+				DCONST_1, DCONST_0, DSUB
+		});
+		expect(4.0D, new byte[] {
+				DCONST_1, DCONST_1, DADD, DCONST_1, DCONST_1, DADD, DMUL
+		});
+		expect(1.0D, new byte[] {
+				DCONST_1, DCONST_1, DADD, DCONST_1, DCONST_1, DADD, DDIV
+		});
+		expect(0.0D, new byte[] {
+				DCONST_1, DCONST_1, DADD, DCONST_1, DCONST_1, DADD, DREM
+		});
+		expect(-1.0D, new byte[] {
+				DCONST_1, DCONST_1, DADD, DCONST_1, DSUB
+		});
+	}
+	@Test
+	void testFloat() {
+		expect(3.0F, new byte[] {
+				FCONST_0, FCONST_1, FCONST_2, FADD, FADD
+		});
+		expect(4.0F, new byte[] {
+				FCONST_1, FCONST_1, FADD, FCONST_1, FCONST_1, FADD, FMUL
+		});
+		expect(1.0F, new byte[] {
+				FCONST_1, FCONST_1, FADD, FCONST_1, FCONST_1, FADD, FDIV
+		});
+		expect(0.0F, new byte[] {
+				FCONST_1, FCONST_1, FADD, FCONST_1, FCONST_1, FADD, FREM
+		});
+		expect(-1.0F, new byte[] {
+				FCONST_1, FCONST_1, FADD, FCONST_1, FSUB
+		});
+	}
+	@Test
+	void testInteger() {
+		expect(-24, new byte[] {
+				ICONST_4, ICONST_3, ICONST_1, ICONST_0, ICONST_M1, IADD, ISUB, IMUL, IMUL
+		});
+		expect(2, new byte[] {
+				ICONST_5, NOP, ICONST_2, IREM
+		});
+		expect(1, new byte[] {
+				ICONST_2, ICONST_5, IREM
+		});
+		expect(0, new byte[] {
+				ICONST_5, ICONST_2, IDIV
+		});
+		expect(2, new byte[] {
+				ICONST_2, ICONST_5, IDIV
+		});
+	}
+	@Test
+	void testInvalid() {
+		final JVM jvm = new JVM();
+		jvm.setBytecode(new byte[] {
+				(byte) 0xff
+		});
+		assertThrows(IllegalStateException.class, jvm::run);
+	}
+	@Test
+	void testLong() {
+		expect(1L, new byte[] {
+				LCONST_0, LCONST_1, LADD
+		});
+		expect(4L, new byte[] {
+				LCONST_1, LCONST_1, LADD, LCONST_1, LCONST_1, LADD, LMUL
+		});
+		expect(1L, new byte[] {
+				LCONST_1, LCONST_1, LADD, LCONST_1, LCONST_1, LADD, LDIV
+		});
+		expect(0L, new byte[] {
+				LCONST_1, LCONST_1, LADD, LCONST_1, LCONST_1, LADD, LREM
+		});
+		expect(-1L, new byte[] {
+				LCONST_1, LCONST_1, LADD, LCONST_1, LSUB
+		});
+	}
+}
